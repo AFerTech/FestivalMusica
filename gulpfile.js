223faed4511f3d79165662fb6f48ce702e76b3e6
Alexis Fernando <alexis96fer@gmail.com>
2022-07-27 23:17:31 -0500

    aligerando img con gulp, formatos avif, webp, jpg

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const {src, dest, watch }= require("gulp");
+const {src, dest, watch, parallel }= require("gulp");
+
 const sass= require("gulp-sass")(require("sass"));
 const plumber= require("gulp-plumber");
+
+const cache= require('gulp-cache');
+const imagemin= require('gulp-imagemin');
+const webp= require('gulp-webp');
+const avif= require('gulp-avif');
+
 
 
 function css(done){
@@ -13,11 +20,52 @@ function css(done){
     done(); //callback avisa a gulp cuando llegamos al final 
 
 }
+
+function imagenes(done){
+    const opcs ={
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opcs)))
+        .pipe(dest('build/img'));
+
+
+    done();
+}   
+
+function versionWebp(done){
+
+    const opcs ={
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opcs))
+    .pipe(dest('build/img'));
+
+
+    done();
+}
+function versionAvif(done){
+
+    const opcs ={
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif(opcs))
+    .pipe(dest('build/img'));
+
+
+    done();
+}
 function dev(done){
-    watch('src/scss/**/*.scss',css)
+    watch('src/scss/**/*.scss',css);
     
     done();
 }
 
 exports.css= css;
-exports.dev=dev;
+exports.imagenes= imagenes;
+exports.versionWebp= versionWebp;
+exports.versionAvif= versionAvif;
+exports.dev=parallel (imagenes, versionWebp,versionAvif,dev);

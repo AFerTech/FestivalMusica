401f9d728b6cd5f4d1a7727a9680b0d6b9cdb595
Alexis Fernando <alexis96fer@gmail.com>
2022-07-28 15:25:04 -0500

    detectando img a la que se le da click para mostrar en grande

--- build/js/app.js
+++ build/js/app.js
@@ -1,25 +1,44 @@
-document.addEventListener('DOMContentLoaded', function(){
-    iniciarApp();
-
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
 });
 
-function iniciarApp(){
-    crearGaleria();
+function iniciarApp() {
+  crearGaleria();
 }
-function crearGaleria(){
-    const galeria=document.querySelector('.galeria-imagenes');
+function crearGaleria() {
+  const galeria = document.querySelector(".galeria-imagenes");
 
-
-    for(let i =1; i<=12;i++){
-        const image= document.createElement('picture');
-        image.innerHTML= `
+  for (let i = 1; i <= 12; i++) {
+    const image = document.createElement("picture");
+    image.innerHTML = `
         
         <source srcset="build/img/thumb/${i}.avif" type="image/avif">
-        <source srcset="build/img/thumb/${i} .webp" type="image/webp">
+        <source srcset="build/img/thumb/${i}.webp" type="image/webp">
         <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" 
         alt="imagen galeria">
         `;
 
-        galeria.appendChild(image);
-    }
+    image.onclick = function () {
+      //callback para identificar a que img se le da click
+      mostrarImagen(i);
+    };
+
+    galeria.appendChild(image);
+  }
+}
+
+function mostrarImagen(id) {
+    const image = document.createElement("picture");
+  image.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" 
+        alt="imagen galeria">
+        `;
+  const overlay = document.createElement("DIV");
+  overlay.appendChild(image);
+  overlay.classList.add("overlay");
+
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
 }

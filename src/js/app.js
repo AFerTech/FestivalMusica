c979034fbba05a5911cf1c89a0727d2e47cf8a61
Alexis Fernando <alexis96fer@gmail.com>
2022-07-30 16:09:01 -0500

    cerrar img al dar click

--- src/js/app.js
+++ src/js/app.js
@@ -28,17 +28,41 @@ function crearGaleria() {
 }
 
 function mostrarImagen(id) {
-    const image = document.createElement("picture");
+  const image = document.createElement("picture");
   image.innerHTML = `
         <source srcset="build/img/grande/${id}.avif" type="image/avif">
         <source srcset="build/img/grande/${id}.webp" type="image/webp">
         <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" 
         alt="imagen galeria">
         `;
+
+  // Crea el overlay con la imagen
   const overlay = document.createElement("DIV");
   overlay.appendChild(image);
   overlay.classList.add("overlay");
+  overlay.onclick= function(){
+    const body=document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  }
 
+  //  Btn para cerrar el modal
+  // const cerrarModal = document.createElement("P");
+  // cerrarModal.textContent = "X";
+  // cerrarModal.classList.add("btn-cerrar");
+
+  // cerrarModal.onclick = function () {
+  //   const body = document.querySelector("body");
+  //   body.classList.remove("fijar-body");
+  //   overlay.remove();
+  // };
+
+  // overlay.appendChild(cerrarModal);
+
+
+  
+  // Añade al HTML
   const body = document.querySelector("body");
   body.appendChild(overlay);
+  body.classList.add("fijar-body");
 }
